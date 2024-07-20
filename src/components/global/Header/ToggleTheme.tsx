572618ab87c398ab57a6e327ec218215676b4a13
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-07-20 16:52:21 +0530

    fixups

--- src/components/global/Header/ToggleTheme.tsx
+++ src/components/global/Header/ToggleTheme.tsx
@@ -3,21 +3,31 @@
 import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import React from "react";
 
 export default function ToggleTheme() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <Button
       variant="secondary"
       size="icon"
       className="rounded-full -mr-1"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      suppressHydrationWarning
     >
       {theme === "dark" ? (
-        <Sun className="h-5 w-5" />
+        <Sun className="h-5 w-5" suppressHydrationWarning />
       ) : (
-        <Moon className="h-5 w-5" />
+        <Moon className="h-5 w-5" suppressHydrationWarning />
       )}
 
       <span className="sr-only">Toggle Theme</span>
